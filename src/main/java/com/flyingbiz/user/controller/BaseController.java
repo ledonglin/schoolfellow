6321e06e63d6d1bc61482d1db25527e58034fa0e
Ledong Lin <ledonglin@gmail.com>
2014-09-02 23:35:06 +0800

    add flyingbiz source folder

---
+++ src/main/java/com/flyingbiz/user/controller/BaseController.java
@@ -0,0 +1,9 @@
+package com.flyingbiz.user.controller;
+
+import org.apache.log4j.Logger;
+
+public class BaseController {
+	private static final Logger logger = Logger.getLogger(BaseController.class);
+
+
+}
